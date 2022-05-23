1f421d4d007af3ae94b65e11bf1d2e1752f91913
simon <simon@dancingcloudservices.com>
2022-05-23 09:58:59 -0600

    Day 1 Break 2

---
+++ src/main/java/usingvar/Ex1.java
@@ -0,0 +1,40 @@
+package usingvar;
+
+public class Ex1 {
+//  var v = 10;
+  // var is ONLY usable for method local variables, NOT class members
+//  class X { var x = 99; }
+  // not unambiguously initialized, hence type is not known
+//  void doStuff(var x) { }
+//  void doStuff() {
+//    // must be initialized IN THE DECLARATION!!
+//    var x;
+//    x = 100;
+//  }
+//  void doStuff() {
+//    var x = 100;
+////    var x = 3.14F;
+////    final long s1 = 100;
+//    // **int** type compile-time constants can be "narrowed"
+//    // for initializations
+////    short s = s1;
+//
+//  }
+//  void doStuff() {
+//    var x = { 1, 2, 3 };
+//  }
+
+//  static void showAll(int [] vals) {
+//  static void showAll(int ... vals) {
+//    for (int x : vals) {
+//      System.out.println("> " + x);
+//    }
+//  }
+//
+//  public static void main(String[] args) {
+//    int [] ia = { 1, 2, 3, 4 };
+////    showAll(ia);
+////    showAll(new int []{ 1, 2, 3, 4 });
+//    showAll(1, 2, 3, 4);
+//  }
+}
